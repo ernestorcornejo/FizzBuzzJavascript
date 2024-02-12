c79b8f64db8641b664d2c8f30e552919964d3cd7
ernestorcornejo <147128656+ernestorcornejo@users.noreply.github.com>
2024-02-11 19:29:01 -0800

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,19 +1,20 @@
-function FizzBuzz(){
-let i = 1;
-while (i < 101) {
-    if(i % 15 == 0){
-    console.log("FizzBuzz");
-}
-    else if(i % 3 == 0) {
-    console.log("Fizz");
+function FizzBuzz(){// Function declaration to hold our logic
+    let i = 1;// Let us use a numeric variable i starting at 1
+    while (i < 101) { // Loop from 1 to 100 using a while check
+        if(i % 15 == 0){
+        // Is divisible by 15?
+        console.log("FizzBuzz"); 
+      } else if(i % 3 == 0) {
+        // If just 3 divides it with no remainder 
+        console.log("Fizz");  
+      } else if(i % 5 == 0) {
+        // Handle if it's a multiple of 5
+        console.log("Buzz"); 
+      } else {  
+        // Default case if none apply  
+        console.log(i);
+      }
+      i++; // Increment i each pass of the loop 
     }
-    else if(i % 5 == 0) {
-    console.log("Buzz");
-    }
-    else {
-    console.log(i);
-    }
-    i++;
-}
-}
-FizzBuzz();
+  }
+  FizzBuzz();// Call our function
